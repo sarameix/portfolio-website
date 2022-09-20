311ca9a844efb6d251dcf0eedb3fea02116f9a6b
smeixner <sarameix22@gmail.com>
2022-09-19 21:19:30 -0400

    finishing first full version of website

--- src/components/Elements/Header.js
+++ src/components/Elements/Header.js
@@ -2,7 +2,7 @@
 // IMPORTS //
 /////////////
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import PDF from '../../documents/SaraMeixner_Resume.pdf';
 
@@ -11,6 +11,12 @@ import PDF from '../../documents/SaraMeixner_Resume.pdf';
 /////////////////////
 
 const Header = () => {
+
+    ///////////////////////
+    // STATE DECLARATION //
+    ///////////////////////
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     // Create Navigation Functionality
     const navigate = useNavigate();
@@ -23,6 +29,10 @@ const Header = () => {
         navigate(event.target.value);
     }
 
+    const toggleHamburgerMenu = (event) => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
     ////////////////////////
     // BODY HTML ELEMENTS //
     ////////////////////////
@@ -30,14 +40,36 @@ const Header = () => {
     return (
         <header>
             <button className='home-button' value="/" onClick={handleRouteSwitch}></button>
-            <div className="header-links">
-                <button className='header-button' value="/About" onClick={handleRouteSwitch}>About Me</button>
-                <a href={PDF} without="true" rel="noopener noreferrer" target="_blank">
-                    <button className='header-button' trailingicon="picture_as_pdf" label="Resume">Resume</button>
-                </a>
-                <button className='header-button' value="/Projects" onClick={handleRouteSwitch}>Projects</button>
-                <button className='header-button' value="/Contact" onClick={handleRouteSwitch}>Contact</button>
+            <div className='header-hamburger' onClick={toggleHamburgerMenu}>
+                {
+                    isMenuOpen ?
+                    <>
+                        <div className='close1'></div>
+                        <div className='close2'></div>
+                    </>
+                    :
+                        <>
+                            <div className='more'></div>
+                            <div className='more'></div>
+                            <div className='more'></div>
+                        </>
+
+                }
             </div>
+            {
+                isMenuOpen ?
+                    <div className='side-nav-menu'>
+                        <button className='header-button' value="/About" onClick={handleRouteSwitch}>About Me</button>
+                        <div className='hamburger-divider'></div>
+                        <a href={PDF} without="true" rel="noopener noreferrer" target="_blank" className='header-button'>Resume</a>
+                        <div className='hamburger-divider'></div>
+                        <button className='header-button' value="/Projects" onClick={handleRouteSwitch}>Projects</button>
+                        <div className='hamburger-divider'></div>
+                        <button className='header-button' value="/Contact" onClick={handleRouteSwitch}>Contact</button>
+                    </div>
+                :
+                    null
+            }
         </header>
     );
 }
