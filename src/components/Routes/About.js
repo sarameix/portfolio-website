f85a0696f19c057222a55ce7615878300d3983e5
smeixner <sarameix22@gmail.com>
2022-09-27 16:56:49 -0400

    changing about p

--- src/components/Routes/About.js
+++ src/components/Routes/About.js
@@ -29,7 +29,7 @@ const About = () => {
                 <h1>About Me</h1>
                 <div className='line'></div>
                 <div className='about-headshot'></div>
-                <p className='about-p'>Hi, my name is Sara Meixner! I'm a 3D Artist and soon-to-be Software Engineer. I graduated in 2020 from Drexel University with a B.S. in Animation in Visual Effects and a minor in Computer Science, and I recently graduated from Genreal Assembly's Software Engineering Immersive Course. My passion lies where art and creativity intersect with technology.</p>
+                <p className='about-p'>Hi, my name is Sara Meixner! I'm a 3D Artist and soon-to-be Software Engineer. I graduated in 2020 from Drexel University with a B.S. in Animation in Visual Effects and a minor in Computer Science. After graduating from the General Assembly's Software Engineering Immersive, I've come to realize my passions lie where art and creativity intersect with technology.</p>
                 <p className='about-p'>I grew up in northern New Jersey, but I am currently living in Philadelphia, PA. Most of my time is spent learning how to code, but in my limited free time, I enjoy reading science fiction novels, making art, cooking meals for friends, and playing with my cat, Lucy.</p>
             </main>
             <Footer />
